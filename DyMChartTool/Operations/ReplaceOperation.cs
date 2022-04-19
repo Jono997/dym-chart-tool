e25889ef7f5f883749c0ba906a5322449b9a72d6
Jono997 <42048866+Jono997@users.noreply.github.com>
2022-04-19 13:17:32 +1000

    Fixed ReplaceOperation bug
    HOW DID THIS SLIP UNDER THE RADAR FOR MONTHS ON END!?!?!??!?

--- DyMChartTool/Operations/ReplaceOperation.cs
+++ DyMChartTool/Operations/ReplaceOperation.cs
@@ -17,8 +17,6 @@ namespace DyMChartTool.Operations
             setTrackFlags(main, left, right);
         }
 
-        public ReplaceOperation(CMapNoteAsset.Type type) : this(type, true, false, false) { }
-
         public ReplaceOperation(float start_time, float end_time, CMapNoteAsset.Type type, bool main, bool left, bool right)
         {
             entire_chart = false;
@@ -27,8 +25,6 @@ namespace DyMChartTool.Operations
             this.type = type;
             setTrackFlags(main, left, right);
         }
-
-        public ReplaceOperation(float start_time, float end_time, CMapNoteAsset.Type type) : this(start_time, end_time, type, true, false, false) { }
 
         private void setTrackFlags(bool main, bool left, bool right)
         {
@@ -46,9 +42,9 @@ namespace DyMChartTool.Operations
             if ((track_flags & MainTrackFlag) > 0)
                 chart.m_notes = applyOnTrack(chart.m_notes);
             if ((track_flags & LeftTrackFlag) > 0)
-                chart.m_notes = applyOnTrack(chart.m_notesLeft);
+                chart.m_notesLeft = applyOnTrack(chart.m_notesLeft);
             if ((track_flags & RightTrackFlag) > 0)
-                chart.m_notes = applyOnTrack(chart.m_notesRight);
+                chart.m_notesRight = applyOnTrack(chart.m_notesRight);
             return chart;
         }
 
